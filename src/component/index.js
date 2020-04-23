1f97069fc716a128dfcbb9dcfa18395aca02db71
weifan <weifan@inexbot.com>
2020-04-23 14:06:47 +0800

    组件封装

---
+++ src/component/index.js
@@ -0,0 +1,23 @@
+import Line from "./line"
+export { Line }
+
+import Bar from "./bar"
+export {Bar}
+
+import Pie from "./pie"
+export {Pie}
+
+import Scatter from "./scatter"
+export {Scatter}
+
+import Map from "./map"
+export {Map}
+
+import Candlestick from "./candlestick"
+export {Candlestick}
+
+import Radar from "./radar"
+export {Radar}
+
+import Boxplot from "./boxplot"
+export {Boxplot}
