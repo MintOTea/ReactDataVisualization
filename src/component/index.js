89ebd688dbdbcdcfbaa88067aba65fed612be4ef
weifan <weifan@inexbot.com>
2020-04-23 14:08:02 +0800

    修bug

--- src/component/index.js
+++ src/component/index.js
@@ -1,23 +1,16 @@
-import Line from "./line"
-export { Line }
-
-import Bar from "./bar"
-export {Bar}
-
-import Pie from "./pie"
-export {Pie}
-
-import Scatter from "./scatter"
-export {Scatter}
-
-import Map from "./map"
-export {Map}
-
-import Candlestick from "./candlestick"
-export {Candlestick}
-
-import Radar from "./radar"
-export {Radar}
-
-import Boxplot from "./boxplot"
-export {Boxplot}
+import Line from "./line";
+import Bar from "./bar";
+import Map from "./map";
+import Pie from "./pie";
+import Boxplot from "./boxplot";
+import Candlestick from "./candlestick";
+import Scatter from "./scatter";
+import Radar from "./radar";
+export { Line };
+export { Bar };
+export { Pie };
+export { Scatter };
+export { Map };
+export { Candlestick };
+export { Radar };
+export { Boxplot };
