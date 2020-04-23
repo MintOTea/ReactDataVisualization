ac2c4c5160ea552e3e55d19e3b81703206189919
fangcaoyun <fangcaoyun@inexbot.com>
2020-04-23 16:02:47 +0800

    yangshi

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
 import React from "react";
 import "./App.css";
-import { Line } from "component";
+import { Row, Col, Button } from "antd";
+import { Line, Bar } from "component";
 import { monthSale } from "data/monthSale";
 import { monthQuality } from "data/monthQuality";
 import { restoringYield } from "data/restoringyield";
 import { monthYield } from "data/monthYield";
 import { malfunction } from "data/malfunction";
+import { OCAPAbnormal } from "data/OCAPAbnormal";
+import { AbnormalProblem } from "data/AbnormalProblem";
 
 function App() {
   return (
-    <div className="App">
-      <div style={{ height: "500px" }}>
-        <p>月度销量</p>
-        <Line data={monthSale} />
-      </div>
-      <div style={{ height: "500px" }}>
-        <p>月度质量表现</p>
-        <Line data={monthQuality} />
-      </div>
-      <div style={{ height: "500px" }}>
-        <p>还原良率</p>
-        <Line data={restoringYield} />
-      </div>
-      <div style={{ height: "500px" }}>
-        <Bar data={monthYield} />
-      </div>
-      <div style={{ height: "500px" }}>
-        <Bar data={malfunction} />
-      </div>
+    <div className="app">
+      <Row>
+        <Col span={6} style={{padding:20}}>
+          <div style={{ height: "48vh" }}>
+            <Bar data={monthYield} />
+          </div>
+          <div style={{ height: "48vh" }}>
+            <Line data={monthSale} />
+          </div>
+        </Col>
+        <Col span={12}>
+          <Row>
+            <Col span={24}>
+              <div style={{ height: "48vh" }}>
+                <Line data={monthQuality} />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <div style={{ height: "48vh" }}>
+                <Line data={restoringYield} />
+              </div>
+            </Col>
+            <Col span={12}>
+              <div style={{ height: "48vh" }}>
+                <Bar data={malfunction} />
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col span={6}>
+          <div style={{ height: "48vh" }}>
+            <Bar data={OCAPAbnormal} />
+          </div>
+          <div style={{ height: "48vh" }}>
+            <Bar data={AbnormalProblem} />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
--- src/data/malfunction.js
+++ src/data/malfunction.js
@@ -6,11 +6,7 @@ export const malfunction = {
       [57.1, 78254, "Milk Tea"],
       [74.4, 41032, "Cheese Cocoa"],
       [50.1, 12755, "Cheese Brownie"],
-      [89.7, 20145, "Matcha Cocoa"],
-      [68.1, 79146, "Tea"],
-      [19.6, 91852, "Orange Juice"],
-      [10.6, 101852, "Lemon Juice"],
-      [32.7, 20112, "Walnut Brownie"],
+      [89.7, 20145, "Matcha Cocoa"]
     ],
   },
   grid: { containLabel: true },
